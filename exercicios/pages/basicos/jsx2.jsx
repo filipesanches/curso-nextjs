fbcd5823e6fed79fb8493843683a077deb43b65a
Filipe Sanches <53585862+filipesanches@users.noreply.github.com>
2024-03-26 19:54:08 +0000

    Next

---
+++ exercicios/pages/basicos/jsx2.jsx
@@ -0,0 +1,9 @@
+export default function jsx2Diferente() {
+  return (
+    <div className="teste">
+      <h1>JSX #02</h1>
+      <p id="teste1">Texto lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p id="teste1">Texto lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+  )
+}
---
+++ exercicios/pages/basicos/jsx4.jsx
@@ -0,0 +1,9 @@
+export default function jsx4() {
+  const subtitulo = 'Hello World! Estou estudando Next.js';
+  return (
+    <div>
+      <h1>integração JSX e JS</h1>
+      <h2>{subtitulo}</h2>
+    </div>
+  );
+}
